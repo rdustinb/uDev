d93f98c9581142aea3e1cbaa7a5ceaf1288d44d5
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-03-21 11:25:09 -0600

    Meeting Sign readState script now prints out a small colored status string.

--- MeetingSign/readState.py
+++ MeetingSign/readState.py
@@ -6,6 +6,21 @@ import configparser
 import os
 import time
 
+# Used to Colorize the state data
+from sty import fg, bg, ef, rs, Style, RgbFg
+
+DEBUG = False
+
+LEDPWMSTEPS = 16
+
+RESET = '\033[m'
+
+def get_color_escape(vals):
+    r = int(255*int(vals[0])/LEDPWMSTEPS)
+    g = int(255*int(vals[1])/LEDPWMSTEPS)
+    b = int(255*int(vals[2])/LEDPWMSTEPS)
+    return '\033[38;2;{};{};{}m'.format(r, g, b)
+
 # For updating the configuation file:
 configFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.ini")
 
@@ -18,26 +33,31 @@ port = list(list_ports.comports())
 for p in port:
   if(p.usb_description() == 'USB Serial'):
     thisDevice = p.device
-    print("I am attempting to read device: %s..."%(thisDevice))
+    if DEBUG: print("I am attempting to read device: %s..."%(thisDevice))
 
 try:
     # Setup and Write to the Serial Device
     serialIF = Serial(thisDevice, config.get('DEFAULT', 'arduinoSerialBaud'), timeout=0.5)
-
+    
     # Send a read command, will get back 64 bytes
     serialIF.write('R.\r\n'.encode('raw_unicode_escape'))
-
+    
     # Get the response...
     thisResponse = serialIF.readline().decode('UTF-8').split(",")
     if int(thisResponse[-1]) == 0:
-        print(thisResponse[:-1])
+        print(get_color_escape(thisResponse[ 0: 3]) + "o" +       
+              get_color_escape(thisResponse[ 3: 6]) + "o" +       
+              get_color_escape(thisResponse[ 6: 9]) + "o" +       
+              get_color_escape(thisResponse[ 9:12]) + "o" +       
+              get_color_escape(thisResponse[12:15]) + "o" +       
+              get_color_escape(thisResponse[15:18]) + "o" +       
+              get_color_escape(thisResponse[18:21]) + "o" +       
+              get_color_escape(thisResponse[21:24]) + "o" + RESET)
     else:
         print("Read of state failed.")
-
+    
     # Close the IF
     serialIF.close()
 except Exception as e:
     print(e)
     print("Reading the meeting sign through the USB interface failed...")
-
-# Decode what the colors are?
